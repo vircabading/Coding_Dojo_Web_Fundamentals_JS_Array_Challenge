2ecd33ed5745d76f133ff253279ad2f92084847d
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-23 07:35:49 -0700

    finished 1st array challenge

--- algorithm.js
+++ algorithm.js
@@ -6,3 +6,29 @@
 import { print_desc } from "./module.js";
 import { print_ln } from "./module.js";
 
+/////////////////////////////////////////////////
+// Functions
+
+function alwaysHungry (tempArray) {
+    var foundFood = false;
+    for (var x=0; x<tempArray.length; x++) {
+        if (tempArray[x] == 'food') {
+            console.log("yummy");
+            foundFood = true;
+        }
+    }
+    if (!foundFood) {
+        console.log("I'm hungry");
+    }
+}
+
+/////////////////////////////////////////////////
+// Main Executable Section
+
+// 1. Always Hungry /////////////////////////////
+print_desc("1. Always Hungy");
+
+print_desc("1st alwaysHungry() call");
+alwaysHungry([3.14, "food", "pie", true, "food"]);      // this should console log "yummy", "yummy"
+print_desc("2nd alwaysHungry() call")
+alwaysHungry([4, 1, 5, 7, 2]);                          // this should console log "I'm hungry"
