f9c724b982ee8777a87c04aaf94369179292de8e
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-23 07:08:17 -0700

    initialize

---
+++ algorithm.js
@@ -0,0 +1,8 @@
+/////////////////////////////////////////////////
+// Coding Dojo > Web Fundamentals > Javascript > Array Challenge
+// By:  Virgilio D. Cabading Jr.    October 23, 2021
+/////////////////////////////////////////////////
+
+import { print_desc } from "./module.js";
+import { print_ln } from "./module.js";
+
